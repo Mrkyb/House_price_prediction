9f666929d4b78e752934d07bc61be394adacdc57
Mohamed <134094892+Mrkyb@users.noreply.github.com>
2025-06-10 15:29:04 +0300

    Remove all files from repository

--- house_price/utils.py
+++
@@ -1,3 +0,0 @@
-def house_price(square_feet, bedrooms, bathrooms, neighborhood, year_built):
-        # Your logic to calculate house price
-    return square_feet * 100 + bedrooms * 5000 + bathrooms * 2000  # Example logic
